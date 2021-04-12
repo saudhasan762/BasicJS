27698ced26226ecbd14a6079e2a72345eb9f208c
saudhasan762 <azanali152@gmail.com>
2021-04-12 15:01:01 +0530

    Added: getting head or tail by flipping a coin

---
+++ functional_programs/flip_coin.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+let number = prompt('Enter the number of times to flip');
+for( let i = 1; i <= number ; i++) {
+    let random = Math.random();
+    if(random < 0.5){
+        console.log("Flip number "+i+" Tail");
+    }else{
+        console.log("Flip number "+i+" Head");
+    }
+}
