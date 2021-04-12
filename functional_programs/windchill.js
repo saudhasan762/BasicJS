914df37357d80d7c2fb7e944dd1735de55213647
saudhasan762 <azanali152@gmail.com>
2021-04-12 18:35:41 +0530

    Added: finding value of windchill

---
+++ functional_programs/windchill.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+let t = prompt('Enter value for t');
+let v = prompt('Enter value for v');
+
+
+if(t < 50 && v>3 && v<120 ){
+   let w = 35.75 + 0.6215*t + (0.4275*t - 25.75)*Math.pow(v, 0.16);
+   console.log("Value of wind chill : "+w);
+} else
+    console.log("The formula is not valid")
