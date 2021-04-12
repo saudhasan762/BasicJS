4a3855e3afa51144bb2d2b3c2f1e77d863a40763
saudhasan762 <azanali152@gmail.com>
2021-04-12 18:25:17 +0530

    Added: finding roots of quadratic equation

---
+++ functional_programs/roots.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+let a = prompt('Enter value for a: ');
+let b = prompt('Enter value of b: ');
+let c = prompt('Enter value of c: ');
+
+let delta = b * b - 4 * a * c;
+
+if (delta > 0){
+    root1 = (-b + Math.sqrt(delta)) / (2*a);
+    root2 = (-b - Math.sqrt(delta)) / (2*a);
+    console.log("Roots are: "+root1+", "+root2);
+} else if (delta == 0){
+    root1 = root2 = -b / (2*a);
+    console.log("Roots are: "+root1+", "+root2);
+}else
+    console.log("Roots are not real");
