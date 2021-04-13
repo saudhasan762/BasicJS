30c23ee26cf87d8910de67c2360a9c97bea22328
saudhasan762 <azanali152@gmail.com>
2021-04-13 16:37:38 +0530

    Added: Sorting integer and string using insertion sort

---
+++ algorithm_programs/insertion_sort.js
@@ -0,0 +1,20 @@
+function insertionSort(arr) {
+    let n = arr.length;
+        for (let i = 1; i < n; i++) {
+            let current = arr[i];
+            let j = i-1; 
+            while ((j >= 0) && (arr[j] > current)) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+    return arr;
+}
+
+function sort() {
+    let arr = [55, 46, 78, 92, 12, 45, 67, 20];
+    let newarr = insertionSort(arr);
+    console.log(newarr);
+}
+sort();
